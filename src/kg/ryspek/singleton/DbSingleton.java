bb1c3f6570ff2d6e23c15cefad3f3055f391c243
Ryspek Baibosunov <ryspek.baybosunov@gmail.com>
2022-05-25 11:42:48 +0600

    add static singleton

---
+++ src/kg/ryspek/singleton/DbSingleton.java
@@ -0,0 +1,13 @@
+package kg.ryspek.singleton;
+
+public class DbSingleton {
+
+    private static DbSingleton instance = new DbSingleton();
+
+    private DbSingleton() {}
+
+    public static DbSingleton getInstance() {
+        return instance;
+    }
+
+}
